9e112c88ba97b0b2d7a36121bf7e34c3f0c551ea
Mikkel <mikkelseptiano86@gmail.com>
2025-09-22 08:32:52 +0700

    add more prompt

--- lib/gemini_model/gemini_util.dart
+++ lib/gemini_model/gemini_util.dart
@@ -3,5 +3,5 @@ class GeminiData {
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
   static const String apikey = "AIzaSyAHBhEhi8gR5V7Dp7yiLtDanxF0BZW_0Ks";
   static const String prompt =
-      "Generate sebuah resep masakan dan cost bahan baku (estimasi dalam Rp) bedasarkan gambar-gambar ini:";
+      "Generate sebuah resep masakan dan cost bahan baku (estimasi dalam Rp) berdasarkan gambar-gambar sisa bahan baku berikut dengan tambahan bahan baku (jika ada) seminim mungkin tidak jauh dari bahan baku berikut:";
 }
