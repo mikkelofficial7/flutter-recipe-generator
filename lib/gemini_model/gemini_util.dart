5d3c267d935ba51b3df6f2d2c1b5be634832e238
Mikkel <mikkelseptiano86@gmail.com>
2025-09-23 13:50:12 +0700

    fix prompting sentences

--- lib/gemini_model/gemini_util.dart
+++ lib/gemini_model/gemini_util.dart
@@ -3,5 +3,5 @@ class GeminiData {
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
   static const String apikey = "AIzaSyAHBhEhi8gR5V7Dp7yiLtDanxF0BZW_0Ks";
   static const String prompt =
-      "Generate sebuah resep masakan dan cost bahan baku (estimasi dalam Rp) berdasarkan gambar-gambar sisa bahan baku berikut dengan tambahan bahan baku (jika ada) seminim mungkin tidak jauh dari bahan baku berikut:";
+      "Generate sebuah resep masakan, cost bahan baku (estimasi dalam Rp), dan 1 url gambar ilustrasi hasil olahan terletak diatas bahan-bahan berdasarkan gambar-gambar sisa bahan baku berikut dengan tambahan bahan baku (jika ada) seminim mungkin tidak jauh dari bahan baku berikut. Url gambar di replace dengan format replace url dengan ![ilustrasi](https://xxxxx.com)";
 }
