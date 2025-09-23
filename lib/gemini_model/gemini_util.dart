30471c5e10295dd77e4d1863207a43dae5d0c6b3
Mikkel <mikkelseptiano86@gmail.com>
2025-09-23 14:01:55 +0700

    Update gemini_util.dart

--- lib/gemini_model/gemini_util.dart
+++ lib/gemini_model/gemini_util.dart
@@ -1,6 +1,7 @@
 class GeminiData {
+  static const String model = "gemini-2.0-flash";
   static const String url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
+      "https://generativelanguage.googleapis.com/v1beta/models/$model:generateContent";
   static const String apikey = "AIzaSyAHBhEhi8gR5V7Dp7yiLtDanxF0BZW_0Ks";
   static const String prompt =
       "Generate sebuah resep masakan, cost bahan baku (estimasi dalam Rp), dan 1 url gambar ilustrasi hasil olahan terletak diatas bahan-bahan berdasarkan gambar-gambar sisa bahan baku berikut dengan tambahan bahan baku (jika ada) seminim mungkin tidak jauh dari bahan baku berikut. Url gambar di replace dengan format replace url dengan ![ilustrasi](https://xxxxx.com)";
